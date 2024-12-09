a57b545d557842826081c02b4df84c847066739f
CodeBuddySLTNS <renz2arjhay@gmail.com>
2024-12-09 15:48:45 +0800

    added greetings

--- script.js
+++ script.js
@@ -1,6 +1,28 @@
 const audio = new Audio("assets/Perfect_Christmas_Parody.mp3");
-audio.preload = 'auto'
+audio.preload = "auto";
 
+window.onload = async () => {
+  const response = await fetch(
+    "https://rest-api-codebuddy2.onrender.com/api/views",
+    {
+      params: {
+        url: window.hostname
+      }
+    }
+  );
+  const data = await response.json();
+  if (data?.error) {
+    return document.querySelector(".views").innerHTML = `Views: error`;
+  }
+  document.querySelector(".views").innerHTML = `Views: ${data.views}`;
+};
+
+document
+  .getElementById("okay")
+  .addEventListener(
+    "click",
+    () => (document.querySelector(".greetings").style.display = "none")
+  );
 document.body.addEventListener("click", () => audio.play());
 document.body.addEventListener("touchmove", () => audio.play());
 document.body.addEventListener("touchstart", () => audio.play());
